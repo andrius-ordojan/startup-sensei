8c88a8f44aa1d22e6823cbacba72fc943fd52e2e
Andrius Ordojan <aor@templafy.com>
2025-03-20 16:05:05 +0100

    working to add context and move podcast scrapping per function

--- main.go
+++ main.go
@@ -2,26 +2,36 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/gocolly/colly/v2"
 )
 
 // TODO: add rogue startup podcast
-const podcastlink = "https://www.startupsfortherestofus.com/archives"
-
 type episode struct {
 	Title       string `selector:"h1.entry-title"`
 	Content     string `selector:"div.entry-content"`
 	PublishedAt string `selector:"time.entry-time"`
 	Url         string
 }
+type podcast struct {
+	domain          string
+	archivePageLink string
+	episodes        []*episode
+}
 
-// TODO: add graceful shutdown so it it doesn't ruin the datastrucuture when kill signal is sent
-func main() {
-	// TODO: read from local file first so I can continue from where I left off
+func scrapeStartupsForTheRestOfUs() (episode, error) {
+	podcast := podcast{
+		domain:          "www.startupsfortherestofus.com",
+		archivePageLink: "https://www.startupsfortherestofus.com/archives",
+	}
+	fmt.Println(podcast)
+
+	return episode{}, nil
+	const podcastlink = "https://www.startupsfortherestofus.com/archives"
+
 	episodes := []*episode{}
-
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.startupsfortherestofus.com"),
 	)
@@ -43,4 +53,23 @@ func main() {
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
 	enc.Encode(episodes)
+
+	return episode{}, nil
+}
+
+func run() error {
+	// ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	// defer cancel()
+	// TODO: read from local file first so I can continue from where I left off
+
+	scrapeStartupsForTheRestOfUs()
+	return nil
+}
+
+// TODO: add graceful shutdown so it it doesn't ruin the datastrucuture when kill signal is sent
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
