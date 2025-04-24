0093d91504f980887ad8befbb266b241cb1a5bf4
Andrius Ordojan <aor@templafy.com>
2025-04-24 16:23:01 +0200

    addded test to check for duplicate podcasts

---
+++ main_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"testing"
+)
+
+type Pair[T comparable] struct {
+	Key   T
+	Count int
+}
+
+type Counter[T comparable] struct {
+	counts map[T]int
+}
+
+func NewCounter[T comparable]() *Counter[T] {
+	return &Counter[T]{counts: make(map[T]int)}
+}
+
+func (c *Counter[T]) Add(k T) {
+	c.counts[k]++
+}
+
+func (c *Counter[T]) Count(k T) int {
+	return c.counts[k]
+}
+
+func (c *Counter[T]) Keys() []T {
+	keys := make([]T, 0, len(c.counts))
+	for k := range c.counts {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func (c *Counter[T]) Items() map[T]int {
+	out := make(map[T]int, len(c.counts))
+	for k, v := range c.counts {
+		out[k] = v
+	}
+	return out
+}
+
+func (c *Counter[T]) Duplicates() []Pair[T] {
+	dupes := make([]Pair[T], 0)
+	for k, v := range c.counts {
+		if v > 1 {
+			dupes = append(dupes, Pair[T]{Key: k, Count: v})
+		}
+	}
+	return dupes
+}
+
+func TestNoDuplicatePodcasts(t *testing.T) {
+	pods, err := NewPodcasts()
+	if err != nil {
+		t.Fatalf("could not create podcasts: %s", err)
+	}
+
+	counter := NewCounter[string]()
+	for _, p := range pods.Podcasts {
+		for _, e := range p.GetEpisodes() {
+			counter.Add(e.Title)
+		}
+	}
+
+	duplicates := counter.Duplicates()
+	if len(duplicates) > 0 {
+		t.Fatalf("found duplicate podcasts: %v", duplicates)
+	}
+}
